917a8d651077f0d4343a4262a4745c9fded7283b
NicoSM99 <nicolas-sanmartin@hotmail.com>
2022-10-17 19:49:20 -0300

    nuevos cambios

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname + "/views/home.html"))
+});
+
+app.get("/register", function (req, res){
+    res.sendFile(path.join(__dirname + "/views/register.html"));
+  });
+
+app.get("/login", function (req, res){
+    res.sendFile(path.join(__dirname + "/views/login.html"));
+  });
+
+
+
+
+app.listen(process.env.PORT || 5500, ()=>
+    console.log("servidor funcionando 5500")
+);
